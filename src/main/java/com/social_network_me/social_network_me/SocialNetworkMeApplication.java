8fd2dc0058916df31c46a8c11a090432d6ab68b1
mamun-131 <mamun_131@hotmail.com>
2018-05-06 01:12:38 -0400

    First Commit

---
+++ src/main/java/com/social_network_me/social_network_me/SocialNetworkMeApplication.java
@@ -0,0 +1,12 @@
+package com.social_network_me.social_network_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkMeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/social_network_me/social_network_me/SocialNetworkMeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.social_network_me.social_network_me;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocialNetworkMeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
